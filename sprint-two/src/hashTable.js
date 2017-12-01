252b51adae747aece0204e8edafdbb035b487ada
jglass314 <jGlass314@gmail.com>
2017-12-01 11:33:09 -0800

    reimplemented hashTable with arrays instead of objects.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,34 +3,78 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._storageSize = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var obj;
+  var idxArr;
   // check if index points to an undefined
   // if so, point it to a new object
   // else, point it to the object at _.storage[index]
   if(this._storage.get(index) === undefined) {
-    obj = {};
+    idxArr = [];
+    var arr = [];
+    arr.push(k);
+    arr.push(v);
+    idxArr.push(arr);
+    this._storage.set(index,idxArr);
+    this._storageSize++;
   } else {
-    obj = this._storage.get(index);
+    idxArr = this._storage.get(index);
+    // set obj
+    var found = false;
+    idxArr.forEach(function(item) {
+      if(item[0] === k) {
+        item[1] = v;
+        found = true;
+        return;
+      }
+    });
+    // if we didn't find the key
+    if(!found) {
+      var arr = [];
+      arr.push(k);
+      arr.push(v);
+      idxArr.push(arr);
+      this._storageSize++;
+    }
   }
-  // set obj
-  obj[k] = v;
-  // store at _.storage[index]
-  this._storage.set(index,obj);
-
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k];
+  var idxArr = this._storage.get(index);
+  if(idxArr === undefined) {
+    return undefined;
+  }
+  var value;
+  idxArr.forEach(function(item, index) {
+    if(item[0] === k) {
+      value = item[1];
+      return;
+    }
+  });
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage.get(index)[k];
+  var idxArr = this._storage.get(index);
+  if(idxArr === undefined) {
+    return;
+  }
+  var idxArrIdx = -1;
+  idxArr.forEach(function(item, idx) {
+    if(item[0] === k) {
+      idxArrIdx = idx;
+      return;
+    }
+  });
+  if(idxArrIdx !== -1) {
+    idxArr.splice(idxArrIdx, 1);
+    this._storageSize--;
+  }
 };
 
 
