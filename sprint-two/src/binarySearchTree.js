c782da57a5696bcc8c15a5aea3896b0d36f34241
jglass314 <jGlass314@gmail.com>
2017-12-01 14:59:19 -0800

    completed binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,57 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree.prototype);
+  bst.left = null;
+  bst.right = null;
+  bst.value = value;
+  return bst;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  if(this.value === value) {
+    return;
+  }
+  if(value > this.value) {
+    if(this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    if(this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if(this.value === value) {
+    return true;
+  }
+  if(value > this.value) {
+    if(this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  } else if(value < this.value) {
+    if(this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if(this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
+  if(this.right !== null) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 
