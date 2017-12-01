0c330e4ad51a1c968af290ad1062549ce2dd499e
jglass314 <jGlass314@gmail.com>
2017-11-30 21:16:30 -0800

    completed set and pseudocode for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // check if index points to an undefined
+  // if so, point it to a new linkedList and add and object {k,v} to the tail
+  // if not, add it to the tail of the existing LL
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // store linkedList at index in a temp variable and retrieve list.head
+  // iterate through LL looking for k
+  // return value
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // store LL at index in temp variable and retrieve list.head
+  // iterate through LL looking for node before k
+    // if list.head has key k, call removeHead
+    // else, remove next node in LL
 };
 
 
@@ -22,5 +32,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
